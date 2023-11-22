1b6c54e2a009999f2a88d8583d9b67bb1e89f785
Steven Yeu <stevenhanhyeu@gmail.com>
2023-11-22 12:18:55 -0800

    consolidated setup into one shell script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
+## Homebrew etup
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/opt/homebrew/bin/brew bundle install
+
+## Script setup
+mkdir -p $HOME/.local/bin
+cp ./scripts/*.sh $HOME/.local/bin
+
+## Zsh setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-
+mv ./.zshrc $HOME/.zshrc
