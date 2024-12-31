75f7c240800e2d048e23cfbbf9291273972c448f
Steven Yeu <stevenhanhyeu@gmail.com>
2024-12-31 00:44:06 -0800

    update sessionizer with fzf bg color changes

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -1,5 +1,12 @@
 # Taken from 
 # https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer
+export FZF_DEFAULT_OPTS="
+	--color=fg:#908caa,bg:-1,hl:#ebbcba
+	--color=fg+:#e0def4,bg+:-1,hl+:#ebbcba
+	--color=border:#403d52,header:#31748f,gutter:-1
+	--color=spinner:#f6c177,info:#9ccfd8,separator:#403d52
+	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
