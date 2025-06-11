b8a37f01e2aca45f90806aece0c4e8593f3c87a5
Steven Yeu <stevenhanhyeu@gmail.com>
2025-06-11 12:12:52 -0700

    update gvm install to use bash

--- setup/03_lang.sh
+++ setup/03_lang.sh
@@ -1,5 +1,5 @@
 ## Install gvm
-zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 
 ## Install rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
