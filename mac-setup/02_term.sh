7c428e481252cb4f443522989ceb8274c24238d6
Steven Yeu <stevenhanhyeu@gmail.com>
2026-03-22 21:43:40 -0700

    update macos setup

--- mac-setup/02_term.sh
+++ mac-setup/02_term.sh
@@ -9,13 +9,13 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
 
 ## Bat
 bat cache --build
 
 ## Configure git fzf
-# git clone https://github.com/junegunn/fzf-git.sh.git $HOME/Repos/fzf-git.sh
+git clone https://github.com/junegunn/fzf-git.sh.git $HOME/Repos/fzf-git.sh
 
 ## Copy over wallpapers
 # git clone https://github.com/JaKooLit/Wallpaper-Bank.git $HOME/Pictures/Wallpapers
