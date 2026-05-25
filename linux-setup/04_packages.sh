edf8e5ee09fd5e210b2e6492364e71d17f8304cd
Steven Yeu <stevenhanhyeu@gmail.com>
2026-05-25 12:03:20 -0700

    linux: updated pacman packages

--- linux-setup/04_packages.sh
+++ linux-setup/04_packages.sh
@@ -1,7 +1,8 @@
-paru -S nvim stow ghostty yazi uv tmux swayidle noctalia-shell lazygit starship zoxide lazydocker \
-    docker docker-compose nemo nemo-share nemo-fileroller solaar delfin \
-    qt6ct nwg-look adw-gtk-theme transmission-gtk transmission-cli \
-    ivpn ivpn-ui gvfs-smb nvtop obsidian postman-bin discord_arch_electron cliphist \
+paru -S nvim stow ghostty yazi uv tmux noctalia-shell lazygit starship zoxide lazydocker \
+    docker docker-compose nemo nemo-share nemo-fileroller solaar tsukimi-bin \
+    qt6ct nwg-look adw-gtk-theme transmission-cli \
+    ivpn ivpn-ui gvfs-smb nvtop postman-bin discord_arch_electron cliphist \
     wlsunset fwupd 1password 1password-cli komikku otf-geist-mono-nerd nyaa-bin \
     ddcci-driver-linux-dkms ddcutil eza ouch f2-bin bluetui trash-cli reflector \
-    appimagelauncher-bin openconnect zmk-studio-bin fzf helium-browser-bin 
+    appimagelauncher-bin openconnect zmk-studio-bin fzf helium-browser-bin okular tree-sitter fwupd \
+    mediainfo sox qbittorrent tree resterm p7zip sqlc tree-sitter-cli zen-browser-bin
