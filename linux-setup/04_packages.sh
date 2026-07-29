098150fc8a353e83f68ee9c16add2478cbe7888e
Steven Yeu <stevenhanhyeu@gmail.com>
2026-07-28 23:39:45 -0700

    noctalia: added settings, linux-setup: split pkgs into categories

--- linux-setup/04_packages.sh
+++ linux-setup/04_packages.sh
@@ -1,8 +1,28 @@
-paru -S nvim stow ghostty yazi uv tmux noctalia-shell lazygit starship zoxide lazydocker \
-    docker docker-compose nemo nemo-share nemo-fileroller solaar tsukimi-bin \
-    qt6ct nwg-look adw-gtk-theme transmission-cli \
-    ivpn ivpn-ui gvfs-smb nvtop postman-bin discord_arch_electron cliphist \
-    wlsunset fwupd 1password 1password-cli komikku otf-geist-mono-nerd nyaa-bin \
-    ddcci-driver-linux-dkms ddcutil eza ouch f2-bin bluetui trash-cli reflector \
-    appimagelauncher-bin openconnect zmk-studio-bin fzf helium-browser-bin okular tree-sitter fwupd \
-    mediainfo sox qbittorrent tree resterm p7zip sqlc tree-sitter-cli zen-browser-bin
+# paru -S nvim stow ghostty yazi uv tmux noctalia-shell lazygit starship zoxide lazydocker \
+#     docker docker-compose nemo nemo-share nemo-fileroller solaar tsukimi-bin \
+#     qt6ct nwg-look adw-gtk-theme transmission-cli \
+#     ivpn ivpn-ui gvfs-smb nvtop postman-bin discord_arch_electron cliphist \
+#     wlsunset fwupd 1password 1password-cli komikku otf-geist-mono-nerd nyaa-bin \
+#     ddcci-driver-linux-dkms ddcutil eza ouch f2-bin bluetui trash-cli \
+#     appimagelauncher-bin openconnect zmk-studio-bin fzf helium-browser-bin tree-sitter \
+#     mediainfo sox qbittorrent tree p7zip sqlc tree-sitter-cli zen-browser-bin
+
+
+# System Utils
+paru -S  noctalia-shell nemo nemo-share nemo-fileroller solaar qt6ct gvfs-smb wlsunset fwupd \
+    ddcci-driver-linux-dkms ddcutil bluetui trash-cli openconnect \
+    p7zip nwg-look adw-gtk-theme nvtop transmission-cli
+    
+
+# Dev
+paru -S nvim stow ghostty yazu uv tmux lazygit starship zoxide lazydocker \
+    docker docker-compose 1password 1password-cli otf-geist-mono-nerd eza \
+    ouch f2-bin fzf tree-sitter tree-sitter-cli tree cliphist sqlc bat
+
+# Media
+paru -S mediainfo sox qbittorrent  nyaa-bin tsukimi-bin delphin
+#komikku
+
+# Apps
+paru -S zen-browser-bin helium-browser-bin zmk-studio-bin appimagelauncher-bin \
+    discord_arch_electron ivpn ivpn-ui postman-biN
